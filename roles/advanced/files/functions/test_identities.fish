e698acf7dce244d755bcbc0d17cbb1f44da68925
Jamesjue <canjian.myself@gmail.com>
2017-03-03 17:05:48 -0500

    update emacs, fix some fish install condition

--- roles/advanced/files/functions/test_identities.fish
+++ roles/advanced/files/functions/test_identities.fish
@@ -4,7 +4,7 @@
 function test_identities
     ssh-add -l | grep "The agent has no identities" > /dev/null
     if [ $status -eq 0 ]
-        ssh-add "~/.ssh/id_(nice_hostname)"
+        ssh-add "~/.ssh/id_rsa"
         if [ $status -eq 2 ]
             start_agent
         end
