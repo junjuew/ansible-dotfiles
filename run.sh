21151dcf9e147c560ce5f09f3b2a7b9a9650bb19
Jamesjue <canjian.myself@gmail.com>
2017-05-22 20:05:47 -0400

    all-in-one script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -ex
+
+echo "running the scripts for hosts:"
+cat hosts
+
+echo "Please enter the username with sudo privelege"
+read -p "Username:" username
+read -s -p "Password:" passwd
+
+export ANSIBLE_ENV_CONFIG_USER=${username}
+export ANSIBLE_ENV_CONFIG_PS=${passwd}
+ansible-playbook -i hosts site.yml
