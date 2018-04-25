f7d65048459f5e7d4b32c440d13f17f2b7dd771f
Junjue Wang <junjuew@cs.cmu.edu>
2018-04-25 14:47:42 -0400

    add in user management

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #! /bin/bash -e
 
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -s|--user_setup)
+            setup_user=true
+            shift
+            ;;
+        *)  # unknown option
+            ;;
+    esac
+    shift # past argument or value
+done
+
 echo "install dependencies from ansible-galaxy..."
 ansible-galaxy install -r requirements.yml
 
@@ -7,11 +21,23 @@ echo "running the scripts for hosts:"
 cat hosts
 
 echo "Please enter the username with sudo privelege from the deploy machines"
-read -p "Username:" username
-read -s -p "Password:" passwd
+read -p "Existing user with sudo priveleges: " existing_user
 
-export ANSIBLE_ENV_CONFIG_USER=${username}
-export ANSIBLE_ENV_CONFIG_PS=${passwd}
+if [ "$setup_user" = true ] ; then
+    echo "setup_user FLAG is set. We will proceed to create a user first."
+    echo "Please enter the username and password you want to create"
+    read -p "Username:" username
+    read -s -p "Password:" passwd
+    printf "\n"
+    read -p "Github Account Name to get authorized_keys from:" github_username
+    export ANSIBLE_ENV_CONFIG_USER=${username}
+    export ANSIBLE_ENV_CONFIG_PS=${passwd}
+    export ANSIBLE_ENV_CONFIG_GITHUB_USER=${github_username}
 
-echo "Running ansible playbook..."
-ansible-playbook -i hosts site.yml
+    echo "Running ansible playbook to create USER ${username}"
+    ansible-playbook -i hosts site.yml -u ${existing_user} --tags must_run user_setup
+    existing_user=${username}
+fi
+
+echo "Running ansible playbook as USER ${existing_user}"
+ansible-playbook -i hosts site.yml -u ${existing_user} --skip-tags user_setup
