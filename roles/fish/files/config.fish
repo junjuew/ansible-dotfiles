b7215dc3ec05f437269c3978929a3ddb1a3e154a
junjuew <junjuew@cs.cmu.edu>
2019-10-19 23:34:46 -0400

    [fish] update config.fish

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,11 +1,27 @@
+# set up fish alias
+if test -e ~/.config/fish/aliases.fish
+    source ~/.config/fish/aliases.fish
+end
+
 # set up editor
 if type -q vim
     set -gx EDITOR vim
 end
 
-# set up fish alias
-if test -e ~/.config/fish/aliases.fish
-    . ~/.config/fish/aliases.fish
+# set up bash variables if bass is available
+if type -q bass
+    test -e ~/.bashrc_custom; and bass source ~/.bashrc_custom
+end
+
+# set up conda
+if test -d "$HOME/miniconda/bin"
+    source "$HOME/miniconda/etc/fish/conf.d/conda.fish"
+    conda activate
+end
+
+# set up direnv hook
+if type -q direnv
+    eval (direnv hook fish)
 end
 
 # Start SSH Agent if it is not already running, and add the
@@ -26,20 +42,4 @@ else
     else
         start_agent
     end
-end
-
-# set up bash variables if bass is available
-if type -q bass
-    test -e ~/.bashrc_custom; and bass source ~/.bashrc_custom
-end
-
-# set up conda
-if test -d "$HOME/miniconda/bin"
-    source "$HOME/miniconda/etc/fish/conf.d/conda.fish"
-    conda activate
-end
-
-# set up direnv hook
-if type -q direnv
-    eval (direnv hook fish)
 end
